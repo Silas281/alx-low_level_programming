2d53c3afd1e063cdafc874170db783259e308905
Silas Sangmin <sangminsilas@gmail.com>
2022-10-18 17:22:58 -0700

    Add a function to print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet - Prints alphabet in lowercase
+*
+*Return: 0 sucess
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

f6c86fd7dec871050f6458077bd6abb1217f16e8
Silas Sangmin <sangminsilas@gmail.com>
2022-11-08 17:10:11 -0800

    A newly allocated mem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - This function returns a pointer to  new created space in memory
+ * @str: string
+ * Return: pointer to newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	int length, i;
+
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+
+	}
+
+	length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+
+	}
+
+	arr = malloc(length * sizeof(char) + 1);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+
+	}
+
+	arr[i] = '\0';
+
+	return (arr);
+
+}
